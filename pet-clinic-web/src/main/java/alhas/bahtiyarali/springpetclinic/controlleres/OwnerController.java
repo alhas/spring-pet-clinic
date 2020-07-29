7bbc69ff49072b58cf868f74717e44f1a0b37359
alhas <bahtiyaralialhas@gmail.com>
2020-07-29 23:48:01 +0300

    Adding Index Page and Controller for Owner Closes. #13

---
+++ pet-clinic-web/src/main/java/alhas/bahtiyarali/springpetclinic/controlleres/OwnerController.java
@@ -0,0 +1,20 @@
+package alhas.bahtiyarali.springpetclinic.controlleres;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping({"owners/"})
+@Controller
+public class OwnerController {
+
+
+    @RequestMapping({"","/", "owners/index", "owners/index.html"})
+    public String listOwners() {
+
+        return "owners/index";
+
+
+    }
+
+
+}
