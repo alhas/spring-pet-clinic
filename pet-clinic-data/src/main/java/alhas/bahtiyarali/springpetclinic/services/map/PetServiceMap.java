8c053188112f84d0e84baa6b36a7755851d0cbb2
alhas <bahtiyaralialhas@gmail.com>
2020-07-29 22:07:44 +0300

    Impl Map based Services... Closes #6

---
+++ pet-clinic-data/src/main/java/alhas/bahtiyarali/springpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,40 @@
+package alhas.bahtiyarali.springpetclinic.services.map;
+
+import alhas.bahtiyarali.springpetclinic.model.Pet;
+import alhas.bahtiyarali.springpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+
+        super.delete(object);
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+        super.deleteById(id);
+
+
+    }
+}
